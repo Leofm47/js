7ac93aa611bd9ca50be82787d6aefdcbafbead7c
Leonardo Falcão de Matos <143974506+Leofm47@users.noreply.github.com>
2023-11-16 18:13:16 -0300

    Add files via upload

---
+++ atividade/index.js
@@ -0,0 +1,39 @@
+/*let nome
+let cidade
+
+console.log(typeof nome, typeof cidade)
+
+nome = prompt("Qual seu nome: ")
+cidade = prompt("Qual sua cidade: ")
+
+console.log(typeof nome, typeof cidade)
+-------------------------------------------------------------------------------------------*/
+
+/*console.log(`Olá meu nome é ${nome} e eu moro em ${cidade}`)
+
+
+const pergunta1 = prompt("Você mora no Brasil? ")
+const pergunta2 = prompt("O melhor filme do mundo é 'emoji o filme'? ")
+const pergunta3 = prompt("Você gosta de JavaScript? ")
+
+console.log(`Você mora no Brasil? ${pergunta1} \n O melhor filme do mundo é 'emoji o filme'? ${pergunta2} \n Você gosta de JavaScript? ${pergunta3} `)
+-------------------------------------------------------------------------------------------*/
+
+/*let x = 13
+let y = 22
+let t = x
+
+x = y
+y = t
+-------------------------------------------------------------------------------------------*/
+
+let valor = prompt("digite um valor: ")
+let valor2 = prompt("digite outro valor: ")
+
+const valorNumero = Number(valor)
+const valor2Numero = Number(valor2)
+
+const soma = valorNumero + valor2Numero
+const multiplicacao = valorNumero * valor2Numero
+
+console.log(`A soma dos números é: ${soma} \n A multiplicação dos números é: ${multiplicacao}` )
